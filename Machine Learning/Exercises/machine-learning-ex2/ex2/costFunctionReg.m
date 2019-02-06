b193433530bc60ec69270cffde1d4816068a6e57
Charles M Dellinger <charles.dellinger@gmail.com>
2019-02-06 13:40:26 -0500

    Machine Learning - Exercise 2 - fixed costFunctionReg.m
    fixed cost function calculation for multi-variable logistic regression

--- Machine Learning/Exercises/machine-learning-ex2/ex2/costFunctionReg.m
+++ Machine Learning/Exercises/machine-learning-ex2/ex2/costFunctionReg.m
@@ -18,10 +18,12 @@ grad = zeros(size(theta));
 %               derivatives of the cost w.r.t. each parameter in theta
 
 
+h = sigmoid(X * theta);
+theta_0 = [0;theta(2:end)]; % `theta` with zero replacing first element
+J = 1/m * (-y'*log(h)-(1-y)'*log(1-h)) + lambda/(2*m) * theta_0' * theta_0;
+grad = 1/m * X'*(h-y) + lambda/m .* theta_0;
 
-
-
-
+     
 % =============================================================
 
 end
