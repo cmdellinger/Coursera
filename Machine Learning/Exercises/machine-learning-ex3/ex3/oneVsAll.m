598804002931ae164f950d95e2c9bedaddf2872d
Charles M Dellinger <charles.dellinger@gmail.com>
2019-02-08 13:48:34 -0500

    Machine Learning - updated oneVsAll.m algorithm
    updated oneVsAll.m algorithm for a neural network using logistic regression

--- Machine Learning/Exercises/machine-learning-ex3/ex3/oneVsAll.m
+++ Machine Learning/Exercises/machine-learning-ex3/ex3/oneVsAll.m
@@ -49,17 +49,22 @@ X = [ones(m, 1) X];
 %                 initial_theta, options);
 %
 
+% the sample code is pretty much what is needed in combination with the hint
+% to use a `for` loop with range 1:num_labels.
 
+% Set Initial theta
+initial_theta = zeros(n + 1, 1);    
+ 
+% Set options for fminunc
+options = optimset('GradObj', 'on', 'MaxIter', 50);
 
-
-
-
-
-
-
-
-
-
+% Run fmincg to obtain the optimal theta
+% This function will return theta and the cost 
+for c = 1:num_labels
+  [theta] = fmincg (@(t)(lrCostFunction(t, X, (y == c), lambda)), ...
+                    initial_theta, options);
+  all_theta(c,:) = theta';
+endfor
 % =========================================================================
 
 
