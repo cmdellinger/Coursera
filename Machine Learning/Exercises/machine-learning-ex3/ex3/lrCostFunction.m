cf670ac7787c13a63a878640742b592a45e29bd1
Charles M Dellinger <charles.dellinger@gmail.com>
2019-02-09 14:21:18 -0500

    Machine Learning - exercise 3 - lrCostFunction.m solution

--- Machine Learning/Exercises/machine-learning-ex3/ex3/lrCostFunction.m
+++ Machine Learning/Exercises/machine-learning-ex3/ex3/lrCostFunction.m
@@ -38,11 +38,10 @@ grad = zeros(size(theta));
 
 
 
-
-
-
-
-
+h = sigmoid(X * theta);
+theta_0 = [0;theta(2:end)]; % `theta` with zero replacing first element
+J = 1/m * (-y'*log(h)-(1-y)'*log(1-h)) + lambda/(2*m) * theta_0' * theta_0;
+grad = 1/m * X'*(h-y) + lambda/m .* theta_0;
 
 
 % =============================================================
